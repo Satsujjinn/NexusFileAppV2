4735bf68bfe7479ffe984f1e95cdc88281718711
Satsujjinn <leonjordaan10@gmail.com>
2025-06-09 09:48:56 +0200

    Add settings view and category card; update color extensions

--- NexusFileApp/Utilities/Color+Nexus.swift
+++ NexusFileApp/Utilities/Color+Nexus.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 
 extension Color {
-    static let nexusGreen = Color("NexusGreen")
-    static let nexusBackground = Color("NexusBackground")
+    /// Primary brand color used throughout the app.
+    static var nexusGreen: Color { Color("NexusGreen") }
+
+    /// Background color that adapts to light and dark mode.
+    static var nexusBackground: Color { Color("NexusBackground") }
 }
